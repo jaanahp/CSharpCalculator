287964af7c307e2dd62f6d04d45c5f09c5fc6b47
jaanahp <pusa.jaana@gmail.com>
2023-02-08 15:34:19 +0200

    Program working according to assignment requirements

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # CSharpCalculator
 C# Calculator, object oriented programming course spring 2023
+***
+Tehtävänanto:
+Toteuta laskin wpf-sovelluksena
+ikkunassa painikkeet 1 - 9 ja =, sekä tuloksen näyttö
+laskee yhteen 2 lukua (arvoltaan 1-9)
+(optional: myös - *ja / )
+Käyttö: paina numeroa(tulee näyttöön), paina toista numeroa(tulee näyttöön) paina =, tulos näyttöön
+palauta projektikansio zipattuna
+
+Compile a wpf-calculator app. Wpf window has buttons 1 - 9 and =
+and a display for showing the result.
+The calculator sums two digits (between 1-9)
+(optional: include -, * and /)
+How to use the app: press a number (number is shown on display),
+press another number (number is shown on display), press = and
+the result is shown on display. Retun a zipped project file.
+***
---
+++ WpfLaskin/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfLaskin
+{
+    public partial class MainWindow : Window
+    {
+
+        //Toteuta laskin wpf-sovelluksena
+        //ikkunassa painikkeet 1 - 9 ja =, sekä tuloksen näyttö
+        //laskee yhteen 2 lukua(arvoltaan 1-9)
+        //(optional: myös - *ja / )
+        //Käyttö: paina numeroa(tulee näyttöön), paina toista numeroa(tulee näyttöön) paina =, tulos näyttöön
+        //Optional, lisätty - * ja /, käyttö toteutettu seuraavasti mukaillen yllä annettua: paina ensimmäistä numeroa (tulee näyttöön),
+        //paina toista numeroa (tulee näyttöön), paina haluttua operaattoria (tulee näyttöön), paina "=" ja tulos tulee näyttöön
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //variables needed in more than one method
+        List<double> numbers = new List<double>();
+        double result = 0;
+        
+        //method for buttons 1-9
+        private void numberBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = (Button)sender;
+            string value = btn.Content.ToString();
+            numbers.Add(Double.Parse(value));
+            outputBox.Text = value;
+        }
+        //method for operator buttons + - * /
+        private void btnOperator_Click(object sender, RoutedEventArgs e)
+        {
+            Button opBtn = (Button)sender;
+            string opValue = opBtn.Content.ToString();
+            outputBox.Text = opValue;
+            if (numbers.Count() == 2)
+            {
+                switch(opValue)
+                {
+                    case "+":
+                        result = numbers[0] + numbers[1];
+                        numbers.Clear();
+                        break;
+                    case "-":
+                        result = numbers[0] - numbers[1];
+                        numbers.Clear();
+                        break;
+                    case "*":
+                        result = numbers[0] * numbers[1];
+                        numbers.Clear();
+                        break;
+                    case"/":
+                        result = numbers[0] / numbers[1];
+                        numbers.Clear();
+                        break;
+                }
+            }
+            else
+            {
+                outputBox.Text = "Give only two numbers";
+                numbers.Clear();
+            }
+        }
+
+
+        private void btnResult_Click(object sender, RoutedEventArgs e)
+        {
+            outputBox.Text = result.ToString();
+        }
+    }
+}
